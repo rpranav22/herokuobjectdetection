505b0d1ff3b00d20228b01d73e54df85388f842d
Pranav <rpranav22@gmail.com>
2019-06-09 16:08:51 +0530

    live detection working

--- ObjectDetector.py
+++ ObjectDetector.py
@@ -18,45 +18,47 @@ class Detector:
         COLORS = np.random.uniform(0, 255, size=(len(CLASSES), 3))
 
         success, frame = self.video.read()
-        # We are using Motion JPEG, but OpenCV defaults to capture raw images,
-        # so we must encode it into JPEG in order to correctly display the
-        # video stream.
-        frame = imutils.resize(frame, width=400)
 
-        # grab the frame dimensions and convert it to a blob
-        (h, w) = frame.shape[:2]
-        blob = cv.dnn.blobFromImage(cv.resize(frame, (300, 300)),
-                                     0.007843, (300, 300), 127.5)
+        if success is True:
+            # print(success, frame)
+            # We are using Motion JPEG, but OpenCV defaults to capture raw images,
+            # so we must encode it into JPEG in order to correctly display the
+            # video stream.
+            frame = imutils.resize(frame, width=400)
 
-        # pass the blob through the network and obtain the detections and
-        # predictions
-        self.net.setInput(blob)
-        detections = self.net.forward()
+            # grab the frame dimensions and convert it to a blob
+            (h, w) = frame.shape[:2]
+            blob = cv.dnn.blobFromImage(cv.resize(frame, (300, 300)),
+                                         0.007843, (300, 300), 127.5)
 
-        for i in np.arange(0, detections.shape[2]):
-            # extract the confidence (i.e., probability) associated with
-            # the prediction
-            confidence = detections[0, 0, i, 2]
+            # pass the blob through the network and obtain the detections and
+            # predictions
+            self.net.setInput(blob)
+            detections = self.net.forward()
 
-            # filter out weak detections by ensuring the `confidence` is
-            # greater than the minimum confidence
-            if confidence > 0.5:
-                # extract the index of the class label from the
-                # `detections`, then compute the (x, y)-coordinates of
-                # the bounding box for the object
-                idx = int(detections[0, 0, i, 1])
-                box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
-                (startX, startY, endX, endY) = box.astype("int")
+            for i in np.arange(0, detections.shape[2]):
+                # extract the confidence (i.e., probability) associated with
+                # the prediction
+                confidence = detections[0, 0, i, 2]
 
-                # draw the prediction on the frame
-                label = "{}: {:.2f}%".format(CLASSES[idx],
-                                             confidence * 100)
-                cv.rectangle(frame, (startX, startY), (endX, endY),
-                              COLORS[idx], 2)
-                y = startY - 15 if startY - 15 > 15 else startY + 15
-                cv.putText(frame, label, (startX, y),
-                            cv.FONT_HERSHEY_SIMPLEX, 0.5, COLORS[idx], 2)
+                # filter out weak detections by ensuring the `confidence` is
+                # greater than the minimum confidence
+                if confidence > 0.5:
+                    # extract the index of the class label from the
+                    # `detections`, then compute the (x, y)-coordinates of
+                    # the bounding box for the object
+                    idx = int(detections[0, 0, i, 1])
+                    box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+                    (startX, startY, endX, endY) = box.astype("int")
 
+                    # draw the prediction on the frame
+                    label = "{}: {:.2f}%".format(CLASSES[idx],
+                                                 confidence * 100)
+                    cv.rectangle(frame, (startX, startY), (endX, endY),
+                                  COLORS[idx], 2)
+                    y = startY - 15 if startY - 15 > 15 else startY + 15
+                    cv.putText(frame, label, (startX, y),
+                                cv.FONT_HERSHEY_SIMPLEX, 0.5, COLORS[idx], 2)
         ret, jpeg = cv.imencode('.jpg', frame)
         return jpeg.tobytes()
 
